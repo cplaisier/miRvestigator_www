600f29f86331314628a3e850d9af1ab7890acd74
Christopher Bare <christopherbare@gmail.com>
2010-12-01 13:10:34 -0800

    fixing up status page reporting params 3

--- server/mirv_db.py
+++ server/mirv_db.py
@@ -114,7 +114,6 @@ def read_parameters(job_uuid):
         annotated_sequences = 0
         for row in result_set:
             genes += 1
-            log("row[1] = " + str(row[1]))
             if (row[1]==1):
                 annotated_sequences += 1
 
