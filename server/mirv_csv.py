feba9683b4ecebfe1f63ec9df0251131d0edce6d
Christopher Bare <christopherbare@gmail.com>
2010-12-10 10:06:34 -0800

    adding csv download

--- server/mirv_csv.py
+++ server/mirv_csv.py
@@ -4,9 +4,9 @@ import mirv_db
 # take sites as a list of dictionaries and returns a csv string
 # each site is a dictionary w/ keys: gene, start, match, site
 def sites_to_csv(sites):
-    s = "Entrez Gene ID, Sequence of Site, Start Relative to Stop Codon (bp), %% Similarity to Consensus Motif\n"
+    s = "Entrez Gene ID,Sequence of Site,Start Relative to Stop Codon (bp),% Similarity to Consensus Motif\n"
     for site in sites:
-        s += "%s, %s, %s, %s\n" % (site['gene'], site['site'], site['start'], site['match'])
+        s += "%s,%s,%s,%s\n" % (site['gene'], site['site'], site['start'], site['match'])
     return s
 
 # takes mirvestigator scores as a list of dictionaries and returns a csv string
@@ -15,8 +15,8 @@ def mirvestigator_scores_to_csv(scores):
     s = "miRNA Name, miRNA Seed, Seed Model, Length of Alignment, Alignment, Viterbi P-Value\n"
     for score in scores:
         statePath = score['statePath']
-        alignment = "xxxxxxx\\r\\n|||||||\\r\\nyyyyyyy"
-        s += "%s, %s, %s, %s, %s, %s\n" % (score['miRNA.name'], score['miRNA.seed'], score['model'], len(score['statePath']), alignment, score['vitPValue'])
+        alignment = "xxxxxxx\n|||||||\nyyyyyyy"
+        s += "%s,%s,%s,%s,\"%s\",%s\n" % (score['miRNA.name'], score['miRNA.seed'], score['model'], len(score['statePath']), alignment, score['vitPValue'])
     return s
 
 def get_sites_as_csv(motif_id):
