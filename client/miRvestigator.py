cf4a20091e27852cb6b234f9c17f691c48884b5a
Christopher Bare <christopherbare@gmail.com>
2010-12-15 14:58:10 -0800

    no motifs

--- client/miRvestigator.py
+++ client/miRvestigator.py
@@ -303,7 +303,7 @@ def results(req):
     # Make a table that looks like this:
     # motif consensus | top miRNA | seed | length | vitPValue | # seqs with site (% of seqs with site)
     if (len(motifs)==0):
-        s+= '<div style="text-align:center;background:#333333;color:#cc0000;height:10ex;font-weight:bold;padding-top:5ex;">No motifs found.</div>'
+        s+= '<div style="text-align:center;background:#333333;color:#cc0000;font-weight:bold;padding-top:6ex;padding-bottom:6ex;">No motifs found.</div>'
     for motif in motifs:
         scoreList = read_mirvestigator_scores(motif['motif_id'])
         if topRet=='all':
