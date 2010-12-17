07548400b18e63e1d6f5e3b2eaa875cf42e683a6
Christopher Bare <christopherbare@gmail.com>
2010-12-16 18:10:45 -0800

    multispecies

--- server/mirv_db.py
+++ server/mirv_db.py
@@ -231,7 +231,7 @@ def get_job_status(job_uuid):
             result['created_at'] = row[1].strftime('%Y.%m.%d %H:%M:%S')
             result['updated_at'] = row[2].strftime('%Y.%m.%d %H:%M:%S')
             result['status'] = row[3]
-            log("row[4] = \"" + row[4] + "\"")
+            log("row[4] = \"" + str(row[4]) + "\"")
             if (row[4] is not None):
                 result['status_message'] = row[4]
         return result
