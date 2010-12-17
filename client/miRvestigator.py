c6d9733eaf991ad5e143c267e7a3daccd6a3d749
Christopher Bare <christopherbare@gmail.com>
2010-12-16 17:42:53 -0800

    multispecies

--- client/miRvestigator.py
+++ client/miRvestigator.py
@@ -120,12 +120,13 @@ def submitJob(req):
     job['s8'] = str(req.form.getfirst('seedModel_8',''))
     #job['bgModel'] = str(req.form.getfirst('bgModel',''))
     job['species'] = str(req.form.getfirst('species',''))
-    print sys.stderr, "}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"
-    print sys.stderr, "}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"
-    print sys.stderr, "}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}"
-    print sys.stderr, "species = " + job['species']
+
+    sys.stderr.write("}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}\n")
+    sys.stderr.write("}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}\n")
+    sys.stderr.write("}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}\n")
+    sys.stderr.write("species = " + job['species'] + "\n")
     sys.stderr.flush()
-    
+
     job['wobble'] = str(req.form.getfirst('wobble',''))
     job['cut'] = str(req.form.getfirst('cut',''))
     job['m6'] = str(req.form.getfirst('motif_6',''))
