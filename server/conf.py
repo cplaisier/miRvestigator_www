dd4e3f926308e29aff3c8354a096ff08737acc1f
Wei-ju Wu <weiju.wu@gmail.com>
2014-04-07 16:18:48 -0700

    updated configuration

--- server/conf.py
+++ server/conf.py
@@ -1,8 +1,8 @@
 # customize file locations
-data_dir='/local/mirvestigator/data'
+data_dir='/local/miRvestigator_www/data'
 viral_species_filename=data_dir+'/organism.txt'
 mirna_filename=data_dir+'/mature.fa.gz'
-pssm_images_dir='/local/mirvestigator/client/images/pssms'
-tmp_dir='/local/mirvestigator/tmp'
+pssm_images_dir='/local/miRvestigator_www/client/images/pssms'
+tmp_dir='/local/miRvestigator_www/tmp'
 
 
